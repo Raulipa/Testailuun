e4a5b64b4db4969ecf120ac8b0460a67601b55d8
raulipa <rauli.paakkala1@gmail.com>
2021-10-11 11:22:09 +0300

    first commit

---
+++ yksi.js
@@ -0,0 +1,8 @@
+var    x = 50;
+var    order = '<h1>Beer</h1>';
+if   (x > 50) {    
+     document.write("He's over 50!" );
+     } else if   (x <= 50 && x > 30){     
+         document.write("<strong> Middle aged man</strong>, who ordered some "+order);
+        } else
+             document.write("It seems you're bit underaged.");
